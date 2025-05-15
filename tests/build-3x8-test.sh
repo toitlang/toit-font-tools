fc3f8326e83a674e9aaf6ab37776b50d386b9f81
Florian Loitsch <florian@toit.io>
2025-05-15 14:30:48 +0200

    Update packages and use 'toit'. (#18)

--- tests/build-3x8-test.sh
+++ tests/build-3x8-test.sh
@@ -5,16 +5,15 @@
 
 set -e
 
-TOIT_RUN=$1
-TOIT_COMPILE=$2
+TOIT=$1
 
 pwd
 
-echo $TOIT_RUN bin/convertfont.toit --doc-comments -- tests/3x8proportional.bdf "Digital Clock 3x8 proportional" build/3x8proportional.toit
-$TOIT_RUN bin/convertfont.toit --doc-comments -- build/toit-font-clock/bdf/3x8proportional.bdf "Digital Clock 3x8 proportional" build/3x8proportional.toit
+echo $TOIT run bin/convertfont.toit -- --doc-comments -- tests/3x8proportional.bdf "Digital Clock 3x8 proportional" build/3x8proportional.toit
+$TOIT run bin/convertfont.toit -- --doc-comments -- build/toit-font-clock/bdf/3x8proportional.bdf "Digital Clock 3x8 proportional" build/3x8proportional.toit
 
-echo $TOIT_COMPILE -Werror --analyze build/3x8proportional.toit
-$TOIT_COMPILE -Werror --analyze build/3x8proportional.toit
+echo $TOIT analyze -Werror build/3x8proportional.toit
+$TOIT analyze -Werror build/3x8proportional.toit
 
 echo cmp build/3x8proportional.toit tests/gold/3x8proportional.toit
 cmp build/3x8proportional.toit tests/gold/3x8proportional.toit
